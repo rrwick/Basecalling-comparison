5592495a49ee9f7669671a366ae7c8049d053839
Ryan Wick <rrwick@gmail.com>
2017-08-29 12:28:20 +1000

    New results and plots

--- plot_results.R
+++ plot_results.R
@@ -14,7 +14,7 @@ albacore_v2_0_0_assembly <- read_tsv("results/albacore_v2.0.0_assembly.tsv", ski
 
 albacore_v1_1_2_nanopolish <- read_tsv("results/albacore_v1.1.2_nanopolished_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.1.2", "Identity_1.1.2", "Rel_len_1.1.2"))
 albacore_v1_2_6_nanopolish <- read_tsv("results/albacore_v1.2.6_nanopolished_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.2.6", "Identity_1.2.6", "Rel_len_1.2.6"))
-albacore_v2_0_0_nanopolish <- read_tsv(Name = numeric(0), Length_2.0.0 = numeric(0), Identity_2.0.0 = numeric(0), Rel_len_2.0.0 = numeric(0))  # temporary empty table, until Nanopolish supports Albacore v2
+albacore_v2_0_0_nanopolish <- data.frame(Name = numeric(), Length_2.0.0 = numeric(), Identity_2.0.0 = numeric(), Rel_len_2.0.0 = numeric())  # temporary empty table, until Nanopolish supports Albacore v2
 
 
 
@@ -33,7 +33,7 @@ all_nanopolish <- merge(all_nanopolish, albacore_v2_0_0_nanopolish, by=1, all=TR
 
 
 
-# Prepare data frames for ggplot
+# Reformat data frames for ggplot
 read_identities <- all_reads[,c("Name", "Length_2.0.0", "Identity_1.1.2", "Identity_1.2.6", "Identity_2.0.0")]
 colnames(read_identities) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0")
 read_identities <- melt(read_identities, id=c("Name", "Length"))
@@ -50,45 +50,62 @@ assembly_identities <- melt(assembly_identities, id=c("Name", "Length"))
 colnames(assembly_identities) <- c("Read_name", "Length", "Basecaller", "Identity")
 
 nanopolish_identities <- all_nanopolish[,c("Name", "Length_1.2.6", "Identity_1.1.2", "Identity_1.2.6", "Identity_2.0.0")]
-colnames(nanopolish_identities) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6")
+colnames(nanopolish_identities) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0")
 nanopolish_identities <- melt(nanopolish_identities, id=c("Name", "Length"))
 colnames(nanopolish_identities) <- c("Read_name", "Length", "Basecaller", "Identity")
 
 
 
-# Violin plots
-ggplot(read_identities, aes(x = factor(Basecaller), y = Identity, weight = Length, fill = factor(Basecaller))) + 
-  geom_violin(draw_quantiles = c(0.5)) +
-  theme_bw() +
-  scale_y_continuous(expand = c(0, 0),
-                     breaks = seq(0, 100, 5),
-                     minor_breaks = seq(0, 100, 1),
-                     labels = scales::unit_format("%")) +
-  coord_cartesian(ylim=c(65, 100))
+# Make aligned proportion data frame
+albacore_v1_1_2_aligned = 100.0 * sum(albacore_v1_1_2_reads[albacore_v1_1_2_reads$Identity_1.1.2 != 0.0,]$Length_1.1.2) / sum(albacore_v1_1_2_reads$Length_1.1.2)
+albacore_v1_1_2_unaligned = 100.0 * sum(albacore_v1_1_2_reads[albacore_v1_1_2_reads$Identity_1.1.2 == 0.0,]$Length_1.1.2) / sum(albacore_v1_1_2_reads$Length_1.1.2)
 
-ggplot(read_rel_lengths, aes(x = factor(Basecaller), y = Relative_length, weight = Length, fill = factor(Basecaller))) + 
-  geom_violin(draw_quantiles = c(0.5)) +
-  theme_bw() +
-  scale_y_continuous(expand = c(0, 0),
-                     breaks = seq(0, 200, 2),
-                     minor_breaks = seq(0, 200, 1),
-                     labels = scales::unit_format("%")) +
-  coord_cartesian(ylim=c(92, 108))
+albacore_v1_2_6_aligned = 100.0 * sum(albacore_v1_2_6_reads[albacore_v1_2_6_reads$Identity_1.2.6 != 0.0,]$Length_1.2.6) / sum(albacore_v1_2_6_reads$Length_1.2.6)
+albacore_v1_2_6_unaligned = 100.0 * sum(albacore_v1_2_6_reads[albacore_v1_2_6_reads$Identity_1.2.6 == 0.0,]$Length_1.2.6) / sum(albacore_v1_2_6_reads$Length_1.2.6)
 
-ggplot(assembly_identities, aes(x = factor(Basecaller), y = Identity, weight = Length, fill = factor(Basecaller))) + 
-  geom_violin(draw_quantiles = c(0.5)) +
-  theme_bw() + 
-  scale_y_continuous(expand = c(0, 0),
-                     breaks = seq(0, 100, 0.5),
-                     minor_breaks = seq(0, 100, 0.1),
-                     labels = scales::unit_format("%")) +
-  coord_cartesian(ylim=c(98, 100))
+albacore_v2_0_0_aligned = 100.0 * sum(albacore_v2_0_0_reads[albacore_v2_0_0_reads$Identity_2.0.0 != 0.0,]$Length_2.0.0) / sum(albacore_v2_0_0_reads$Length_2.0.0)
+albacore_v2_0_0_unaligned = 100.0 * sum(albacore_v2_0_0_reads[albacore_v2_0_0_reads$Identity_2.0.0 == 0.0,]$Length_2.0.0) / sum(albacore_v2_0_0_reads$Length_2.0.0)
 
-ggplot(nanopolish_identities, aes(x = factor(Basecaller), y = Identity, weight = Length, fill = factor(Basecaller))) + 
+aligned_proportion <- data.frame(Basecaller = factor(), Aligned = numeric(), Unaligned = numeric())
+aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v1.1.2"), Aligned = albacore_v1_1_2_aligned, Unaligned = albacore_v1_1_2_unaligned))
+aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v1.2.6"), Aligned = albacore_v1_2_6_aligned, Unaligned = albacore_v1_2_6_unaligned))
+aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v2.0.0"), Aligned = albacore_v2_0_0_aligned, Unaligned = albacore_v2_0_0_unaligned))
+
+
+
+# Plots!
+ggplot(read_identities, aes(x = Basecaller, y = Identity, weight = Length, fill = Basecaller)) + 
   geom_violin(draw_quantiles = c(0.5)) +
-  theme_bw() + 
-  scale_y_continuous(expand = c(0, 0),
-                     breaks = seq(0, 100, 0.5),
-                     minor_breaks = seq(0, 100, 0.1),
-                     labels = scales::unit_format("%")) +
-  coord_cartesian(ylim=c(98.5, 100))
+  theme_bw() + guides(fill=FALSE) +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 100, 5), minor_breaks = seq(0, 100, 1), labels = scales::unit_format("%")) +
+  coord_cartesian(ylim=c(65, 100)) +
+  labs(title = "Read identities", x = "", y = "")
+
+ggplot(aligned_proportion, aes(x = Basecaller, y = Unaligned, fill = Basecaller)) +
+  geom_bar(stat='identity') +
+  theme_bw() + guides(fill=FALSE) +
+  scale_y_continuous(expand = c(0, 0), labels = scales::unit_format("%")) +
+  coord_cartesian(ylim=c(0, 0.5)) +
+  labs(title = "Proportion unaligned reads", x = "", y = "")
+
+ggplot(read_rel_lengths, aes(x = Basecaller, y = Relative_length, weight = Length, fill = Basecaller)) + 
+  geom_hline(yintercept = 100) + 
+  geom_violin(draw_quantiles = c(0.5)) +
+  theme_bw() + guides(fill=FALSE) +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 200, 2), minor_breaks = seq(0, 200, 1), labels = scales::unit_format("%")) +
+  coord_cartesian(ylim=c(92, 108)) +
+  labs(title = "Relative read lengths", x = "", y = "")
+
+ggplot(assembly_identities, aes(x = Basecaller, y = Identity, weight = Length, fill = Basecaller)) + 
+  geom_violin(draw_quantiles = c(0.5)) +
+  theme_bw() + guides(fill=FALSE) + 
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 100, 0.5), minor_breaks = seq(0, 100, 0.1), labels = scales::unit_format("%")) +
+  coord_cartesian(ylim=c(98, 100)) +
+  labs(title = "Assembly identities (pre-Nanopolish)", x = "", y = "")
+
+ggplot(nanopolish_identities, aes(x = factor(Basecaller), y = Identity, weight = Length, fill = Basecaller)) + 
+  geom_violin(draw_quantiles = c(0.5)) +
+  theme_bw() + guides(fill=FALSE) + 
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 100, 0.5), minor_breaks = seq(0, 100, 0.1), labels = scales::unit_format("%")) +
+  coord_cartesian(ylim=c(98, 100)) +
+  labs(title = "Assembly identities (post-Nanopolish)", x = "", y = "")
