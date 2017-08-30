060bd44a23e23e010128d9482342cda19f26f627
Ryan Wick <rrwick@gmail.com>
2017-08-30 13:00:03 +1000

    Add Albacore v1.0.4 results

--- plot_results.R
+++ plot_results.R
@@ -1,19 +1,32 @@
 library(readr)
 library(reshape2)
 library(ggplot2)
+library(RColorBrewer)
+
+
+
+# Prepare colours for ggplot
+basecaller_names <- c("Albacore v1.0.4", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0", "Albacore v2.0.1")
+basecaller_colours <- brewer.pal(5, "YlOrRd")
+names(basecaller_colours) <- levels(as.factor(basecaller_names))
+fill_scale <- scale_fill_manual(name = "Basecaller", values = basecaller_colours)
+
 
 
 # Load the tables
+albacore_v1_0_4_reads <- read_tsv("results/albacore_v1.0.4_reads.tsv", skip = 1, col_names = c("Name", "Length_1.0.4", "Identity_1.0.4", "Rel_len_1.0.4"))
 albacore_v1_1_2_reads <- read_tsv("results/albacore_v1.1.2_reads.tsv", skip = 1, col_names = c("Name", "Length_1.1.2", "Identity_1.1.2", "Rel_len_1.1.2"))
 albacore_v1_2_6_reads <- read_tsv("results/albacore_v1.2.6_reads.tsv", skip = 1, col_names = c("Name", "Length_1.2.6", "Identity_1.2.6", "Rel_len_1.2.6"))
 albacore_v2_0_0_reads <- read_tsv("results/albacore_v2.0.0_reads.tsv", skip = 1, col_names = c("Name", "Length_2.0.0", "Identity_2.0.0", "Rel_len_2.0.0"))
 albacore_v2_0_1_reads <- read_tsv("results/albacore_v2.0.1_reads.tsv", skip = 1, col_names = c("Name", "Length_2.0.1", "Identity_2.0.1", "Rel_len_2.0.1"))
 
+albacore_v1_0_4_assembly <- read_tsv("results/albacore_v1.0.4_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.0.4", "Identity_1.0.4", "Rel_len_1.0.4"))
 albacore_v1_1_2_assembly <- read_tsv("results/albacore_v1.1.2_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.1.2", "Identity_1.1.2", "Rel_len_1.1.2"))
 albacore_v1_2_6_assembly <- read_tsv("results/albacore_v1.2.6_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.2.6", "Identity_1.2.6", "Rel_len_1.2.6"))
 albacore_v2_0_0_assembly <- read_tsv("results/albacore_v2.0.0_assembly.tsv", skip = 1, col_names = c("Name", "Length_2.0.0", "Identity_2.0.0", "Rel_len_2.0.0"))
 albacore_v2_0_1_assembly <- read_tsv("results/albacore_v2.0.1_assembly.tsv", skip = 1, col_names = c("Name", "Length_2.0.1", "Identity_2.0.1", "Rel_len_2.0.1"))
 
+albacore_v1_0_4_nanopolish <- read_tsv("results/albacore_v1.0.4_nanopolished_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.0.4", "Identity_1.0.4", "Rel_len_1.0.4"))
 albacore_v1_1_2_nanopolish <- read_tsv("results/albacore_v1.1.2_nanopolished_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.1.2", "Identity_1.1.2", "Rel_len_1.1.2"))
 albacore_v1_2_6_nanopolish <- read_tsv("results/albacore_v1.2.6_nanopolished_assembly.tsv", skip = 1, col_names = c("Name", "Length_1.2.6", "Identity_1.2.6", "Rel_len_1.2.6"))
 albacore_v2_0_0_nanopolish <- data.frame(Name = numeric(), Length_2.0.0 = numeric(), Identity_2.0.0 = numeric(), Rel_len_2.0.0 = numeric())  # temporary empty table, until Nanopolish supports Albacore v2
@@ -22,17 +35,20 @@ albacore_v2_0_1_nanopolish <- data.frame(Name = numeric(), Length_2.0.1 = numeri
 
 
 # Merge the tables together
-all_reads <- albacore_v1_1_2_reads
+all_reads <- albacore_v1_0_4_reads
+all_reads <- merge(all_reads, albacore_v1_1_2_reads, by=1, all=TRUE)
 all_reads <- merge(all_reads, albacore_v1_2_6_reads, by=1, all=TRUE)
 all_reads <- merge(all_reads, albacore_v2_0_0_reads, by=1, all=TRUE)
 all_reads <- merge(all_reads, albacore_v2_0_1_reads, by=1, all=TRUE)
 
-all_assemblies <- albacore_v1_1_2_assembly
+all_assemblies <- albacore_v1_0_4_assembly
+all_assemblies <- merge(all_assemblies, albacore_v1_1_2_assembly, by=1, all=TRUE)
 all_assemblies <- merge(all_assemblies, albacore_v1_2_6_assembly, by=1, all=TRUE)
 all_assemblies <- merge(all_assemblies, albacore_v2_0_0_assembly, by=1, all=TRUE)
 all_assemblies <- merge(all_assemblies, albacore_v2_0_1_assembly, by=1, all=TRUE)
 
-all_nanopolish <- albacore_v1_1_2_nanopolish
+all_nanopolish <- albacore_v1_0_4_nanopolish
+all_nanopolish <- merge(all_nanopolish, albacore_v1_1_2_nanopolish, by=1, all=TRUE)
 all_nanopolish <- merge(all_nanopolish, albacore_v1_2_6_nanopolish, by=1, all=TRUE)
 all_nanopolish <- merge(all_nanopolish, albacore_v2_0_0_nanopolish, by=1, all=TRUE)
 all_nanopolish <- merge(all_nanopolish, albacore_v2_0_1_nanopolish, by=1, all=TRUE)
@@ -40,29 +56,35 @@ all_nanopolish <- merge(all_nanopolish, albacore_v2_0_1_nanopolish, by=1, all=TR
 
 
 # Reformat data frames for ggplot
-read_identities <- all_reads[,c("Name", "Length_2.0.1", "Identity_1.1.2", "Identity_1.2.6", "Identity_2.0.0", "Identity_2.0.1")]
-colnames(read_identities) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0", "Albacore v2.0.1")
+basecaller_identities <- c("Identity_1.0.4", "Identity_1.1.2", "Identity_1.2.6", "Identity_2.0.0", "Identity_2.0.1")
+basecaller_rel_lengths <- c("Rel_len_1.0.4", "Rel_len_1.1.2", "Rel_len_1.2.6", "Rel_len_2.0.0", "Rel_len_2.0.1")
+
+read_identities <- all_reads[,c("Name", "Length_2.0.1", basecaller_identities)]
+colnames(read_identities) <- c("Name", "Length", basecaller_names)
 read_identities <- melt(read_identities, id=c("Name", "Length"))
 colnames(read_identities) <- c("Read_name", "Length", "Basecaller", "Identity")
 
-read_rel_lengths <- all_reads[,c("Name", "Length_2.0.0", "Rel_len_1.1.2", "Rel_len_1.2.6", "Rel_len_2.0.0", "Rel_len_2.0.1")]
-colnames(read_rel_lengths) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0", "Albacore v2.0.1")
+read_rel_lengths <- all_reads[,c("Name", "Length_2.0.1", basecaller_rel_lengths)]
+colnames(read_rel_lengths) <- c("Name", "Length", basecaller_names)
 read_rel_lengths <- melt(read_rel_lengths, id=c("Name", "Length"))
 colnames(read_rel_lengths) <- c("Read_name", "Length", "Basecaller", "Relative_length")
 
-assembly_identities <- all_assemblies[,c("Name", "Length_2.0.0", "Identity_1.1.2", "Identity_1.2.6", "Identity_2.0.0", "Identity_2.0.1")]
-colnames(assembly_identities) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0", "Albacore v2.0.1")
+assembly_identities <- all_assemblies[,c("Name", "Length_2.0.1", basecaller_identities)]
+colnames(assembly_identities) <- c("Name", "Length", basecaller_names)
 assembly_identities <- melt(assembly_identities, id=c("Name", "Length"))
 colnames(assembly_identities) <- c("Read_name", "Length", "Basecaller", "Identity")
 
-nanopolish_identities <- all_nanopolish[,c("Name", "Length_1.2.6", "Identity_1.1.2", "Identity_1.2.6", "Identity_2.0.0", "Identity_2.0.1")]
-colnames(nanopolish_identities) <- c("Name", "Length", "Albacore v1.1.2", "Albacore v1.2.6", "Albacore v2.0.0", "Albacore v2.0.1")
+nanopolish_identities <- all_nanopolish[,c("Name", "Length_1.2.6", basecaller_identities)]
+colnames(nanopolish_identities) <- c("Name", "Length", basecaller_names)
 nanopolish_identities <- melt(nanopolish_identities, id=c("Name", "Length"))
 colnames(nanopolish_identities) <- c("Read_name", "Length", "Basecaller", "Identity")
 
 
 
 # Make aligned proportion data frame
+albacore_v1_0_4_aligned = 100.0 * sum(albacore_v1_0_4_reads[albacore_v1_0_4_reads$Identity_1.0.4 != 0.0,]$Length_1.0.4) / sum(albacore_v1_0_4_reads$Length_1.0.4)
+albacore_v1_0_4_unaligned = 100.0 * sum(albacore_v1_0_4_reads[albacore_v1_0_4_reads$Identity_1.0.4 == 0.0,]$Length_1.0.4) / sum(albacore_v1_0_4_reads$Length_1.0.4)
+
 albacore_v1_1_2_aligned = 100.0 * sum(albacore_v1_1_2_reads[albacore_v1_1_2_reads$Identity_1.1.2 != 0.0,]$Length_1.1.2) / sum(albacore_v1_1_2_reads$Length_1.1.2)
 albacore_v1_1_2_unaligned = 100.0 * sum(albacore_v1_1_2_reads[albacore_v1_1_2_reads$Identity_1.1.2 == 0.0,]$Length_1.1.2) / sum(albacore_v1_1_2_reads$Length_1.1.2)
 
@@ -76,6 +98,7 @@ albacore_v2_0_1_aligned = 100.0 * sum(albacore_v2_0_1_reads[albacore_v2_0_1_read
 albacore_v2_0_1_unaligned = 100.0 * sum(albacore_v2_0_1_reads[albacore_v2_0_1_reads$Identity_2.0.1 == 0.0,]$Length_2.0.1) / sum(albacore_v2_0_1_reads$Length_2.0.1)
 
 aligned_proportion <- data.frame(Basecaller = factor(), Aligned = numeric(), Unaligned = numeric())
+aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v1.0.4"), Aligned = albacore_v1_0_4_aligned, Unaligned = albacore_v1_0_4_unaligned))
 aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v1.1.2"), Aligned = albacore_v1_1_2_aligned, Unaligned = albacore_v1_1_2_unaligned))
 aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v1.2.6"), Aligned = albacore_v1_2_6_aligned, Unaligned = albacore_v1_2_6_unaligned))
 aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("Albacore v2.0.0"), Aligned = albacore_v2_0_0_aligned, Unaligned = albacore_v2_0_0_unaligned))
@@ -86,14 +109,14 @@ aligned_proportion <- rbind(aligned_proportion, data.frame(Basecaller = factor("
 # Plots!
 ggplot(read_identities, aes(x = Basecaller, y = Identity, weight = Length, fill = Basecaller)) + 
   geom_violin(draw_quantiles = c(0.5)) +
-  theme_bw() + guides(fill=FALSE) +
+  fill_scale + theme_bw() + guides(fill=FALSE) +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0, 100, 5), minor_breaks = seq(0, 100, 1), labels = scales::unit_format("%")) +
-  coord_cartesian(ylim=c(65, 100)) +
+  coord_cartesian(ylim=c(70, 100)) +
   labs(title = "Read identities", x = "", y = "")
 
 ggplot(aligned_proportion, aes(x = Basecaller, y = Unaligned, fill = Basecaller)) +
   geom_bar(stat="identity", colour="black") +
-  theme_bw() + guides(fill=FALSE) +
+  fill_scale + theme_bw() + guides(fill=FALSE) +
   scale_y_continuous(expand = c(0, 0), labels = scales::unit_format("%")) +
   coord_cartesian(ylim=c(0, 0.5)) +
   labs(title = "Unaligned reads", x = "", y = "")
@@ -101,7 +124,7 @@ ggplot(aligned_proportion, aes(x = Basecaller, y = Unaligned, fill = Basecaller)
 ggplot(read_rel_lengths, aes(x = Basecaller, y = Relative_length, weight = Length, fill = Basecaller)) + 
   geom_hline(yintercept = 100) + 
   geom_violin(draw_quantiles = c(0.5)) +
-  theme_bw() + guides(fill=FALSE) +
+  fill_scale + theme_bw() + guides(fill=FALSE) +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0, 200, 2), minor_breaks = seq(0, 200, 1), labels = scales::unit_format("%")) +
   coord_cartesian(ylim=c(92, 108)) +
   labs(title = "Relative read lengths", x = "", y = "")
@@ -119,3 +142,4 @@ ggplot(nanopolish_identities, aes(x = factor(Basecaller), y = Identity, weight =
   scale_y_continuous(expand = c(0, 0), breaks = seq(0, 100, 0.5), minor_breaks = seq(0, 100, 0.1), labels = scales::unit_format("%")) +
   coord_cartesian(ylim=c(98, 100)) +
   labs(title = "Assembly identities (post-Nanopolish)", x = "", y = "")
+
